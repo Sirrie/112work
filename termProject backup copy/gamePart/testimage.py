efe0beb789e4ad38d86fcb169863edbc4a74f5b1
Sirrie <siyuchen@andrew.cmu.edu>
2014-08-31 08:34:20 -0400

    remove files

--- termProject backup copy/gamePart/testimage.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-img=cv2.imread("/Users/Sirrie/Desktop/15112/termProject/gamePart/1.jpg")
-print img
-cv2.imshow('test',img)
--- termProject backup copy/project/dialogtest.py
+++
@@ -1,6 +0,0 @@
-import Tkinter
-import tkSimpleDialog
-
-root = Tkinter.Tk()
-var = tkSimpleDialog.askstring("Name prompt", "enter your name")
-print var
